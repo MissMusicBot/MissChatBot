2afa1c0bbd286b71757c97a3be41f6bd1b1e2237
MissMusicBot <khatriuzef197@gmail.com>
2025-06-15 22:18:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "21553328"
+API_ID = "25805829"
 # -------------------------------------------------------------
-API_HASH = "3bcc137f124bc8518c30d461986e247b"
+API_HASH = "f65c202eddcf522bb485d4ab866cd295"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7940019678:AAFnZD4f0dM_PPZm9vbmuv_HGPxw6EjGswo")
+BOT_TOKEN = getenv("BOT_TOKEN", "8105204852:AAGbVa3jK9kvDqytV__FXSJgA6jQyUIzdF0")
 STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://oliva:oliva123@cluster0.6cohqfv.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "6693611573"))
-SUPPORT_GRP = "EsproSupport"
-UPDATE_CHNL = "EsproUpdate"
-OWNER_USERNAME = "Ur_Haiwan"
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://mrhari988i:mrhari988i@cluster0.5gj9cii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/?retryWrites=true&w=majority")
+OWNER_ID = int(getenv("OWNER_ID", "8130271170"))
+SUPPORT_GRP = "sumomll"
+UPDATE_CHNL = "system_SUPPORT8"
+OWNER_USERNAME = "System0999"
