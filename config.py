ed1ae669dc4eca370ce370ec73cb4c974888f18e
MissMusicBot <khatriuzef197@gmail.com>
2025-06-16 12:59:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "f65c202eddcf522bb485d4ab866cd295"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "7257164727:AAEUfb0sMS5suyd636YmwdVP_g3M2FWA3js")
 STRING1 = getenv("STRING_SESSION", None)
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://mrhari988i:mrhari988i@cluster0.5gj9cii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://mongodb+srv://oliva:oliva123@cluster0.6cohqfv.mongodb.net/?retryWrites=true&w=majority/?retryWrites=true&w=majority&appName=Cluster0/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "8130271170"))
 SUPPORT_GRP = "sumomll"
 UPDATE_CHNL = "system_SUPPORT8"
